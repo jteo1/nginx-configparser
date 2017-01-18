05ae23e0706931c5f9d535efa79a58518b6617fe
Justin <jteo1@yahoo.com>
2017-01-17 22:06:05 -0800

    fixed nested directives ending with }}

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -9,3 +9,55 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+//test nested directives
+TEST(NginxConfigParserTest, NestedConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+  bool success = parser.Parse("config_with_nested", &out_config);
+
+  EXPECT_EQ(out_config.statements_.size(), 1);
+  EXPECT_TRUE(success);
+}
+
+TEST(NginxConfigParserTest, LargeConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  EXPECT_TRUE(parser.Parse("large_config", &out_config));
+}
+
+//Test fixture for string parsing
+class NginxStringConfigTest : public ::testing::Test {
+protected:
+    bool parseString(const std::string config_string) {
+        std::stringstream config_stream(config_string);
+        return parser.Parse(&config_stream, &out_config);
+    }
+
+    NginxConfigParser parser;
+    NginxConfig out_config;
+};
+
+//test invalid inputs
+TEST_F(NginxStringConfigTest, InvalidConfig) {
+    EXPECT_FALSE(parseString("foo bar"));
+    //EXPECT_FALSE(parseString("server {listen 80;")); //TODO: fix this!
+}
+
+//test basic valid configs
+TEST_F(NginxStringConfigTest, SimpleConfigs){
+    //test comments read properly
+    EXPECT_TRUE(parseString("foo bar; #a comment"));
+    EXPECT_EQ(out_config.statements_.size(), 1);
+
+    out_config.statements_.clear();
+
+    //test multiple line commands
+    EXPECT_TRUE(parseString("foo\nbar\nx;"));
+    EXPECT_EQ(out_config.statements_.size(), 1);
+    EXPECT_EQ(out_config.statements_[0]->tokens_[0], "foo");
+    EXPECT_EQ(out_config.statements_[0]->tokens_[1], "bar");
+    EXPECT_EQ(out_config.statements_[0]->tokens_[2], "x");
+    EXPECT_EQ(out_config.statements_[0]->tokens_.size(), 3);
+}
